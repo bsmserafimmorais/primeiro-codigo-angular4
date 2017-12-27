790b5aaaec5639fb9fcba7350fbc09b216e5cfb6
Bruno Serafim de Morais <bsmserafim@gmail.com>
2017-12-27 15:25:08 -0200

    primeiro código em angular4

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
 
+import {Student} from './student/student.model';
+
 @Component({
   selector: 'jad-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'jad';
+	
+	students: Student[] = [
+			 { name: 'luke', isJedi: true, temple: 'coruscant'},
+			 { name: 'leia', isJedi: false},
+			 { name: 'han', isJedi: false}
+  ]
 }
